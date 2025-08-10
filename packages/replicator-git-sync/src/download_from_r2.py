5dab9401bbac56bbfaed43803943c8d0c8b297c7
David Fant <david@fant.io>
2025-08-10 13:00:42 -0400

    typescript based replicator-git-sync

--- packages/replicator-git-sync/src/download_from_r2.py
+++
@@ -1,5 +0,0 @@
-async def download_from_r2(bucket, key: str) -> bytes:
-  obj = await bucket.get(key)
-  if not obj:
-    raise Exception(f"Object not found: {key}")
-  return bytes(await obj.to_py().bytes())

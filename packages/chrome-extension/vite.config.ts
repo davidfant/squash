74462a62a73be9c2d914840fc50b0e03f6c5c601
David Fant <david@fant.io>
2025-08-19 21:59:23 -0700

    misc chrome extension

--- packages/chrome-extension/vite.config.ts
+++ packages/chrome-extension/vite.config.ts
@@ -1,28 +1,10 @@
-import manifest from "@/manifest.config";
 import { crx } from "@crxjs/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import manifest from "./src/manifest.config";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), crx({ manifest })],
-  resolve: {
-    alias: { "@": path.resolve(__dirname, "./src") },
-  },
-  server: {
-    // port: 5174,
-    // strictPort: true,
-    // hmr: { protocol: "ws", host: "localhost", port: 5174 },
-    cors: true,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type, Authorization",
-    },
-  },
-  build: {
-    outDir: "dist", // Changed from 'build' to 'dist' - CRXJS expects this
-    sourcemap: true,
-  },
+  plugins: [react(), tailwindcss(), tsconfigPaths(), crx({ manifest })],
 });

437142a0c2230ddf1c363aa7e8eeb4aaa23c44ea
David Fant <david@fant.io>
2025-08-19 21:52:45 -0700

    chrome-extension tsc

--- packages/chrome-extension/vite.config.ts
+++ packages/chrome-extension/vite.config.ts
@@ -1,9 +1,9 @@
+import manifest from "@/manifest.config";
 import { crx } from "@crxjs/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
-import manifest from "./manifest.config";
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), crx({ manifest })],
